e25bfb0e25dcc2b79d1a28832ef97d2b4cfcce08
tt7779 <thomas_tan03@hotmail.com>
2025-06-19 13:12:51 +0800

    Update cpd_categories.php

--- php/cpd_categories.php
+++ php/cpd_categories.php
@@ -146,7 +146,7 @@ $result = $conn->query($sql);
             <div class="col-md-9 col-lg-10 main-content">
                 <div class="container mt-4">
                     <div class="d-flex justify-content-between align-items-center mb-4">
-                        <h2><i class="fas fa-tags me-2"></i>Manage CPD Categories</h2>
+                        <h2><i class="fas fa-tags me-2"></i>Manage Event Categories</h2>
                         <a href="cpd_management.php" class="btn btn-outline-primary">
                             <i class="fas fa-arrow-left me-2"></i>Back to Programs
                         </a>
